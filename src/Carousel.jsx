bf7d8d8a5ffc76000c3bb42fd8bbeb9322a3536e
Chris Mems <christopher.mems@.com>
2023-03-17 14:29:58 -0500

    set constraints for carousel sliding

--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -4,13 +4,21 @@ import images from "./images"
 
 
 const Carousel = () => {
+  const [width, setWidth] = useState(0)
+
+  const carousel = useRef()
+
+  useEffect(() => {
+    setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth)
+  }, [])
+
   return (
     <>
       <motion.div className="carousel">
-        <motion.div drag="x" className="inner-carousel">
-          {images.map((image, idx) => {
+        <motion.div ref={carousel} drag="x" dragConstraints={{ right: 1, left: -width}} className="inner-carousel">
+          {images.map((image) => {
             return (
-              <motion.div className="item" key={idx}>
+              <motion.div className="item" key={image}>
                 <img src={image} alt="resort" />
               </motion.div>
             )
